0c86360544f94ed9e23cdce46093a1e05c5c840f
Jace Ju <jaceju@gmail.com>
2015-04-15 17:33:24 +0800

    Create Song model and migration.

---
+++ app/Song.php
@@ -0,0 +1,12 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+
+    protected $fillable = ['name'];
+
+    public $timestamps = false;
+
+}
---
+++ database/migrations/2015_04_15_091522_create_songs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('songs', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->text('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('songs');
+	}
+
+}
